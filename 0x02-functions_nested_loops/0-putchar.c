b866db9e8b487fce5358b68e7b475142aa5280dd
Hwoods4948 <115465453+Hwoods4948@users.noreply.github.com>
2023-02-21 15:19:15 +0000

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
